8098e61cfa3de36ded6e0728ed10ff3c9dc16bdb
pisi774 <pisi774@gmail.com>
2025-05-08 16:30:10 -0400

    atualização de movimentos do personagem
    atualização de movimentos do personagem

--- medieval-warrior/player/player.gd
+++ medieval-warrior/player/player.gd
@@ -1,14 +1,22 @@
 extends CharacterBody2D
 
+@export var speed: float = 3
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 var is_running: bool = false
 
-func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("ui_accept"):
-		if is_running:
-			animation_player.play("idle")
-			is_running = false
-		else:
-			animation_player.play("run")
-			is_running = true
+#func _process(delta: float) -> void:
+	#if Input.is_action_just_pressed("ui_accept"):
+		#if is_running:
+			#animation_player.play("idle")
+			#is_running = false
+		#else:
+			#animation_player.play("run")
+			#is_running = true
+
+func _physics_process(delta: float) -> void:
+	var input_vector = Input.get_vector("mover_esquerda","mover_direita","mover_cima","mover_baixo")
+	var target_velocity = input_vector * speed * 100.0
+	velocity = lerp(velocity, target_velocity, 0.05)
+	move_and_slide()
+	
